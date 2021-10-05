acb93bc7aa1373727ff7a152c948ad9eb96e4ddc
Deepanshu Mittal <deepmittal02@gmail.com>
2021-10-05 11:19:37 +0530

    wave display

--- level1/Basic of Programming/2DArray/004_StateofWakanda1.java
+++ level1/Basic of Programming/2DArray/004_StateofWakanda1.java
@@ -5,8 +5,8 @@ public class Main{
 
 public static void main(String[] args) throws Exception {
     Scanner scn= new Scanner(System.in);
-    int n= scn.nextInt();
-    int m= scn.nextInt();
+    int n= scn.nextInt(); // number of rows
+    int m= scn.nextInt(); // number of columns
     
     int[][] arr= new int[n][m];
     
@@ -17,12 +17,12 @@ public static void main(String[] args) throws Exception {
     }
     
     for(int j=0;j<m;j++){
-        if(j%2==0){
+        if(j%2==0){  // if columns is even 
             for(int i=0;i<n;i++){
                 System.out.println(arr[i][j]);
             }
         }
-        else {
+        else {  // if column is odd
             for(int i=n-1; i>=0;i--) {
                  System.out.println(arr[i][j]);
             }
