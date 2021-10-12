a6d55a9266964c826b1d3beca51719f4381c287a
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-10-12 20:08:19 +0530

    digits of a num

--- level1/Basic of Programming/Getting Started/DigitOfNumbers.java
+++ level1/Basic of Programming/Getting Started/DigitOfNumbers.java
@@ -5,12 +5,10 @@ public class numberCount {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int n = sc.nextInt();
-	    int i = 0;
 	    List<Integer> values = new ArrayList<Integer>();
 	    while(n>0){
 	    	values.add(n%10);
         n = (int)n/10;
-        i = i+1;
 	}//System.out.println(values);
 	    for (int j = values.size()-1; j>=0; j--) System.out.println(values.get(j));
 
