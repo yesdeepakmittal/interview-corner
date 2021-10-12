00a182fcc8c1a0c905d9f370121bf06f35dac6cb
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-10-12 18:21:21 +0530

    number count

---
+++ level1/Basic of Programming/Getting Started/numberCount.java
@@ -0,0 +1,14 @@
+package dsa;
+import java.util.*;
+public class numberCount {
+
+	public static void main(String[] args) {
+//		String s = new String();
+	    Scanner sc = new Scanner(System.in);
+//	    s = sc.nextLine();
+	    int n = sc.nextInt();
+	    System.out.println(Integer.toString(n).length());
+
+	}
+
+}
