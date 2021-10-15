68ee193d2c5106406ffb0f20e6743d1094894b17
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-10-15 20:04:00 +0530

    wrong ans removed

--- level1/Basic of Programming/Getting Started/inverse.py
+++ level1/Basic of Programming/Getting Started/inverse.py
@@ -2,16 +2,4 @@ n = input()
 l = list(n)
 d = {}
 d1 = {}
-for index,value in enumerate(n):
-    d[index+1] = int(value)
-
-print(d.items())
-for i,j in d.items():
-    d1[j-1] = i
-
-print(d1.items())
-
-for key,value in d1.items():
-    l[key] = value
-
-print(l)
+#------Wrong---------#
