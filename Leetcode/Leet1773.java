413d20c05336f97664c762eb46d84eea530d1064
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-11-02 19:31:33 +0530

    leet

---
+++ Leetcode/Leet1773.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+        if(ruleKey.equals("type")){
+            for(int i = 0; i< items.size(); i++){
+                if(items.get(i).get(0).equals(ruleValue)){
+                    count++;
+                }
+            }
+        }
+        else if(ruleKey.equals("color")){
+            for(int i = 0; i< items.size(); i++){
+                if(items.get(i).get(1).equals(ruleValue)){
+                    count+=1;
+                }
+            }
+        }
+        else {
+            for(int i = 0; i< items.size(); i++){
+                if(items.get(i).get(2).equals(ruleValue)){
+                    count++;
+                }
+            }
+        } return count;
+    }
+}
