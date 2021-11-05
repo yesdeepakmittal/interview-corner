fb9a7cc8e0ab326adc7d20a89699ecfd78144163
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-11-05 21:22:40 +0530

    leet 73 py medium

---
+++ Leetcode/Leet0073.py
@@ -0,0 +1,14 @@
+matrix = [[1,1,0],[1,1,1],[1,1,1]]
+idx = []
+for row in range(len(matrix)):
+    for index,value in enumerate(matrix[row]):
+        if value == 0:
+            matrix[row] = [0 for i in range(len(matrix[row]))]
+            # for col in range(len(matrix)):
+            #     matrix[col][index] = 0  does not work b'coz of running changes
+                # print(matrix[col][index])
+            idx.append(index)
+for i in idx:
+    for row in range(len(matrix)):
+        matrix[row][i] = 0
+print(matrix)
