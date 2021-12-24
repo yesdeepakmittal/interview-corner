e77c3e744cd73e938a3ce4b18cbf6282dfe2edeb
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-12-24 07:32:54 +0530

    minimum cost TC update

--- otherSnippets/MinimumCost.py
+++ otherSnippets/MinimumCost.py
@@ -1,6 +1,8 @@
 ''''
 What is the minimum cost to remove all elements from an array?
 Cost of removing an element is the sum of all elements presend in the array at that time
+
+TC - O(NlogN) - sorting complexitys
 '''
 arr = [9,14,21,7,3,4,26,10]
 arr.sort(reverse=True)
