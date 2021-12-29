ff7dbfa06d4b32ebcff8134695a75e267fd7adb8
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-12-29 21:45:36 +0530

    reduced TC and SC

--- otherSnippets/SumOfAllSubarrays.py
+++ otherSnippets/SumOfAllSubarrays.py
@@ -1,4 +1,6 @@
 #sum of all subarray
+
+'TC - O(N2) | SC - O(N) even after using pf'
 arr = [1,2,3,4,5]
 pf = [0]*(len(arr)+1)
 for i in range(len(arr)):
@@ -9,6 +11,14 @@ sm = 0
 for i in range(len(arr)):
   for j in range(i+1,len(arr)+1):
     sm += pf[j] - pf[i]
+print(sm)
 
 
+'TC - O(N) | SC - O(1) - super'
+sm = 0
+n = len(arr)
+for i in range(n):
+    start_index = i + 1
+    end_index   = n - i
+    sm += arr[i]*(start_index*end_index)
 print(sm)
