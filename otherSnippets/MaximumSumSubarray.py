84a4eada0e67ea596db9316b744c2be1387b50fa
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-12-30 10:08:40 +0530

    kadane algo

--- otherSnippets/MaximumSumSubarray.py
+++ otherSnippets/MaximumSumSubarray.py
@@ -26,3 +26,34 @@ for i in range(len(arr)):
         if s > MaxSum:
             MaxSum = s
 print('MaxSum is ->',MaxSum)
+
+
+'''
+TC - O(N) | SC - O(1)
+'''
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxSubArray(self, arr):
+        import sys
+
+        MaxSum = -sys.maxsize - 1
+
+        # for i in range(len(arr)):
+        #     s = 0
+        #     for j in range(i,len(arr)):
+        #         s += arr[j]
+        #         if s > MaxSum:
+        #             MaxSum = s
+        # return MaxSum
+        
+        #Kadane Algo
+        max_end_here = 0
+        for i in arr:
+            max_end_here += i
+            if MaxSum < max_end_here:
+                MaxSum = max_end_here
+
+            if max_end_here < 0:
+                max_end_here = 0
+        return MaxSum 
