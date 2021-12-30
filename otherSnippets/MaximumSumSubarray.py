9e0737cf2e92358bbe3a57b32f4bec070b297637
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2021-12-30 10:16:55 +0530

    kadane algo

--- otherSnippets/MaximumSumSubarray.py
+++ otherSnippets/MaximumSumSubarray.py
@@ -1,18 +1,4 @@
 arr = [4,-11,1,6,2,3,-2]
-# arr = [2,9,5]
-s = 0
-
-pf = [0]*len(arr)
-pf[0] = arr[0]
-for i in range(1,len(arr)):
-    pf[i] = pf[i-1] + arr[i]
-for i in range(len(arr)):
-    s = s + pf[i]
-    for j in range(i+1,len(arr)):
-        s += pf[j] - pf[i]
-print('Sum of all subarrays:',s)
-
-
 #Maximum sum subarray
 
 '''
