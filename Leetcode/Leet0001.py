467f9238449dcf474b75c71a9a6d783c0fad78df
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2022-01-12 07:54:31 +0530

    Hashing | Complexity reduced

--- Leetcode/Leet0001.py
+++ Leetcode/Leet0001.py
@@ -1,9 +1,17 @@
 #https://leetcode.com/problems/two-sum/
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        # arr = []
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
         
+        # TC - O(N) | SC - O(N) | Hashing 
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [d[nums[i]],i]
+            else:
+                d[target - nums[i]] = i
+        return []
