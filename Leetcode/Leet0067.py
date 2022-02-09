92c768f61b6842333483637ef1228909a47e9052
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2022-02-09 23:57:05 +0530

    leet 0067

--- Leetcode/Leet0067.py
+++ Leetcode/Leet0067.py
@@ -1,3 +1,7 @@
+'''
+Adding binary numbers
+'''
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,base=2) + int(b,base=2))[2:]      
+        return bin(int(a,base=2) + int(b,base=2))[2:] 
