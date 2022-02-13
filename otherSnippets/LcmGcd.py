088646ba8ee9bd0378e6c1d3bbf1b53c006094ce
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2022-02-13 12:52:24 +0530

    gcd for a range of numbers

--- otherSnippets/LcmGcd.py
+++ otherSnippets/LcmGcd.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 class Solution:
     # @param A : integer
     # @param B : integer
@@ -22,3 +25,27 @@ val = 0
 for i in arr:
     val = Solution().gcd(val,i)
 print('GCD',val)
+
+
+## GCD for a range of numbers
+class Solution2:
+    # @param A : string
+    # @param B : string
+    # @return a strings
+    def gcd(self,x,y):
+        if x == 0:
+            return y
+        else:
+            return self.gcd(y%x,x)
+            
+    def solve(self, A, B):
+        val = 0
+        if int(A) > int(B):
+            A,B = int(B),int(A)
+        for i in range(int(A),int(B)+1):
+            val = self.gcd(val,i)
+            if val == 1:   
+                return 1
+        return val
+
+print(Solution2().solve('1234','1111234342'))
