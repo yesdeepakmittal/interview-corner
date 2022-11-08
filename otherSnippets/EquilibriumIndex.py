4d3730db260a9eb64e8a21243366279e1c5bd8b3
Deepak Mittal <deepakmittalbutjevra@gmail.com>
2022-11-08 19:00:23 +0530

    Equilibrium index - reduced TC from 3N to 2N

--- otherSnippets/EquilibriumIndex.py
+++ otherSnippets/EquilibriumIndex.py
@@ -14,24 +14,16 @@ for i in range(len(arr)):
 #Prefix-Sum
 # TC - O(N) | SC - O(N)
 # Space complexity can be reduced to 2N from 3N by calculating pf into arr itself
-pf = [0]*len(arr)
-before_sum = [0]*len(arr)
-after_sum = [0]*len(arr)
 
-pf[0] = arr[0]
+after = [0]*len(arr)
+before = [0]*len(arr)
+
 for i in range(1,len(arr)):
-    pf[i] = pf[i-1] + arr[i]
+    before[i] = before[i-1] + arr[i-1]
 
-before_sum[0] = 0
-for i in range(1,len(arr)):
-    before_sum[i] = pf[i-1]
-
-
-after_sum[-1] = 0
-#After sum for index 0 is range[1,6] which is pf[6] - pf[1 - 1]
-for i in range(len(arr) - 1):
-    after_sum[i] = pf[len(arr) - 1] - pf[i]
+for i in range(len(arr) - 2, -1, -1):
+    after[i] = after[i+1] + arr[i+1]
 
 for i in range(len(arr)):
-    if before_sum[i] == after_sum[i]:
+    if before[i] == after[i]:
         print(i)
